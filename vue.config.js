f0e50a222619d1b61b2da57fbd33eedcec481100
fw <yuelafeng@163.com>
2019-02-14 13:36:40 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const webpack = require('webpack')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
+  lintOnSave: true,
+  //设置跨域代理
+  devServer: {
+    port: 3001,
+    proxy: {
+     
+    }
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        // JS: "JS",
+      })
+    ]
+  },
+  //链式调用修改webpack配置
+  chainWebpack: config => {
+    //设置alias
+    config.resolve.alias
+      .set('@$', resolve('src'))
+  },
+ 
+}
